b171535d075b3e7cc0ed27ff35f28526bb1277e3
Lucas Lopes <lucas.lopesx1@gmail.com>
2021-10-26 13:06:54 -0300

    Update api.ts

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const url =
-  'https://api.themoviedb.org/3/trending/all/day?api_key=dc35ea9a8f93b99ec2a67ce9ce0238b2';
+  'https://api.themoviedb.org/3/movie/popular?api_key=dc35ea9a8f93b99ec2a67ce9ce0238b2&language=en-US&page=1';
 
 const api = axios.create({
   baseURL: url,
